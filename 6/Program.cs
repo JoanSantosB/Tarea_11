724892dd7ff5c1b580e904a4327869509b8e0197
JoanSantosB <cjoanbatz@gmail.com>
2026-03-06 23:02:06 -0600

    Termine el ejercicio 6

---
+++ 6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    internal class Program
+    {
+        static void Saludar(string nombre)
+        {
+            Console.WriteLine("Hola "+nombre);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un nombre: ");
+            string name=Console.ReadLine();
+            Saludar(name);
+        }
+    }
+}
