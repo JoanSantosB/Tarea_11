eee13fda516e81840c2470b65bfbb73663063137
JoanSantosB <cjoanbatz@gmail.com>
2026-03-06 22:40:20 -0600

    Agregar archivos de proyecto.

---
+++ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Saludar()
+        {
+            Console.WriteLine("Hola, bienvenido");
+        }
+        static void Main(string[] args)
+        {
+            Saludar();
+        }
+    }
+}
