a71da8b9f79af62d43b33cab898224fecccedb84
JoanSantosB <cjoanbatz@gmail.com>
2026-03-06 23:28:06 -0600

    Terminé el ejercicio 10

---
+++ 10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10
+{
+    internal class Program
+    {
+        static void ImprimirTexto(string texto,int veces)
+        {
+            for(int i = 0; i < veces; i++)
+            {
+                Console.WriteLine($"{i+1}-{texto}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el texto: ");
+            string sentence = Console.ReadLine();
+            Console.Write("Ingrese las veces que se imprima el texto: ");
+            int cantidad = int.Parse(Console.ReadLine());
+            ImprimirTexto(sentence,cantidad);
+        }
+    }
+}
