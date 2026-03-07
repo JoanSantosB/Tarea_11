ddf6acca36abcf4d6378bbd73b115b29bfcb5b40
JoanSantosB <cjoanbatz@gmail.com>
2026-03-06 22:46:13 -0600

    Termine el ejercicio 2

---
+++ 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    internal class Program
+    {
+        static void MostrarNombre()
+        {
+            Console.Write("Ingrese un nombre: ");
+            string name=Console.ReadLine();
+            Console.WriteLine(name);
+        }
+        static void Main(string[] args)
+        {
+            MostrarNombre();
+        }
+    }
+}
