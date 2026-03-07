0f4ec402598e5fde7a002e7797ffdbcb40ca949b
JoanSantosB <cjoanbatz@gmail.com>
2026-03-06 23:21:55 -0600

    Terminé el ejercicio 9

--- 9/Program.cs
+++ 9/Program.cs
@@ -8,9 +8,18 @@ namespace _9
 {
     internal class Program
     {
-
+        static void SumarMostrar(int a,int b)
+        {
+            int resultado = a+b;
+            Console.WriteLine($"La suma es: {resultado}");
+        }
         static void Main(string[] args)
         {
+            Console.Write("Ingrese el primer número: ");
+            int number1=int.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            int number2=int.Parse(Console.ReadLine());
+            SumarMostrar(number1,number2);
         }
     }
 }
