8dace6de678f688103730adb6bd426e1633a217e
JoanSantosB <cjoanbatz@gmail.com>
2026-03-06 23:50:39 -0600

    Terminpe el ejercicio 15

---
+++ 14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    internal class Program
+    {
+        static double Dividir(double a, double b)
+        {
+            double resultado = a / b;
+            Console.WriteLine($"El resultado es: {resultado}");
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el primer número: ");
+            double number1=double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo número: ");
+            double number2=double.Parse(Console.ReadLine());
+            if (number2 == 0)
+            {
+                Console.WriteLine("No se puede dividir entre 0");
+            }
+            else
+            {
+                Dividir(number1, number2);
+            }
+        }
+    }
+}
---
+++ 15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    internal class Program
+    {
+        static bool EsPar(int a)
+        {
+            bool resultado=false;
+            if (a % 2 == 0)
+            {
+                Console.WriteLine($"{a} es un número par");
+                resultado = true;
+            }
+            else
+            {
+                Console.WriteLine($"{a} es un número impar");
+                resultado = false;
+            }
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int number=int.Parse(Console.ReadLine());
+            EsPar(number);
+        }
+    }
+}
