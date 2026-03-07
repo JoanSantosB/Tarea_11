17370b366fbc0e740d298c751532edeac4285512
JoanSantosB <cjoanbatz@gmail.com>
2026-03-06 23:13:44 -0600

    Terminé el ejercicio 8

---
+++ 8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    internal class Program
+    {
+        static int MostrarCuadrado(int n)
+        {
+            int resultado = n*n;
+            Console.WriteLine($"El cuadrado del número es: {resultado}");
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+            int number=int.Parse(Console.ReadLine());
+            MostrarCuadrado(number);
+        }
+    }
+}
