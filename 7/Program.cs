7462dabb9094af04fadde56ad2688c9f4abd9277
JoanSantosB <cjoanbatz@gmail.com>
2026-03-06 23:08:22 -0600

    Terminé el ejercicio 7

---
+++ 7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    internal class Program
+    {
+        static int MostrarDoble(int n)
+        {
+            int resultado = n * 2;
+            Console.WriteLine($"El doble es: {resultado}");
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int number=int.Parse(Console.ReadLine());
+            MostrarDoble(number);
+        }
+    }
+}
