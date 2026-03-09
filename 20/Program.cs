fc2e19cf25fd5fd376e062534acc37ef42cada57
JoanSantosB <cjoanbatz@gmail.com>
2026-03-08 20:54:48 -0600

    Terminá el ejercicio 20

---
+++ 20/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20
+{
+    internal class Program
+    {
+        static int ContarLetras(string palabra)
+        {
+            int contador = 0;
+            foreach (char c in palabra)
+            {
+                contador++; 
+            }
+            Console.WriteLine($"Total de letras: {contador}");
+            return contador;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una palabra: ");
+            string word = Console.ReadLine();
+            ContarLetras(word);
+        }
+    }
+}
