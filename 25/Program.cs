00bec820186af613bf858f843ab3f181e054eb63
JoanSantosB <cjoanbatz@gmail.com>
2026-03-08 21:25:48 -0600

    Terminé el ejercicio 25

---
+++ 25/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25
+{
+    internal class Program
+    {
+        static bool EsPrimo(int n)
+        {
+            bool primo = true;
+            if (n <= 1)
+            {
+                primo = false;
+            }
+            else
+            {
+                for (int i = 2; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        primo = false; break;
+                    }
+                }
+            }
+            if( primo)
+            {
+                Console.WriteLine($"El número {n} es un número primo");
+            }
+            else
+            {
+                Console.WriteLine($"El número {n} no es un número primo");
+            }
+            return primo;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número: ");
+            int number = int.Parse(Console.ReadLine());
+            if (number >= 0)
+            {
+                EsPrimo(number);
+            }
+            else
+            {
+                Console.WriteLine("El número debe ser un número positivo");
+            }
+        }
+    }
+}
