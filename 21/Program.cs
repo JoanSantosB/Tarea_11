37e5849a9196affbc114f8ed453b26beb554b0ca
JoanSantosB <cjoanbatz@gmail.com>
2026-03-08 21:02:33 -0600

    Terminé el ejercicio 21

---
+++ 21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21
+{
+    internal class Program
+    {
+        static int ContarVocales(string texto)
+        {
+            int contador = 0;
+            foreach (char c in texto)
+            {
+                switch (c)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        contador++;
+                        break;
+                }
+            }
+            Console.WriteLine($"Total de vocales: {contador}");
+            return contador;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una palabra: ");
+            string word=Console.ReadLine().ToLower();
+            ContarVocales(word);
+        }
+    }
+}
